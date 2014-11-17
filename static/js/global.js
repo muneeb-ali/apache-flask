19059ddb258e4b483f635f34f8fa1b00845f35bd
Muneeb Ali <muneeb@ali.vc>
2014-11-17 18:19:19 -0500

    static files

---
+++ static/js/global.js
@@ -0,0 +1,9 @@
+//-------------------------------------
+// Author: Muneeb Ali - http://muneeb.org
+//-------------------------------------
+
+//try test_func('hello world')
+var test_func = function(input) {
+  
+  alert(input)
+};
